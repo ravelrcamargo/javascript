5edbba327f7c3b2bf1145c05f5714e9f705b7ba8
ravelrcamargo <ravelrcamargo@gmail.com>
2023-06-29 17:46:04 -0300

    js

---
+++ cursojs/aula11/ex008.js
@@ -0,0 +1,7 @@
+
+var vel = 59 
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+    if(vel > 60){//condição simples
+        console.log('Você foi MULTADO!!')
+    }
+console.log('dirija sempre usando cinto de segutança')
---
+++ cursojs/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'italy'
+console.log(`vivendo em ${pais}`)
+    if(pais == 'brasil'){
+    console.log('Você é Brasileiro')
+    } else{
+        console.log('Você é estrangeiro')
+    }
