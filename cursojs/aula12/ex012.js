f806a88a9e265e203f724ccdd0d2a8911455cbb9
ravelrcamargo <ravelrcamargo@gmail.com>
2023-07-01 19:42:17 -0300

    up

---
+++ cursojs/aula12/ex012.js
@@ -0,0 +1,14 @@
+var agora = new Date()
+var hora = agora.getHours()
+
+console.log(`agora são extamente ${hora} horas`)
+
+if(hora <= 12 && hora >= 6){
+    console.log('Bom dia')
+}else if(hora <=18){
+    console.log('boa tarde')
+}
+else if(hora > 18 || hora < 6){
+    console.log('boa noite')
+}
+
